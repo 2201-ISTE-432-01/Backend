d1ec119b71125177ecfaf41baf279942a14a1969
Avery <afn1947@rit.edu>
2020-11-08 15:53:37 -0500

    implimented genre and track

--- src/model/genre.js
+++ src/model/genre.js
@@ -5,32 +5,30 @@ const database = require ('../database');
 //////////
 
 exports.create = async function(data) {
-    // TODO Avery
-    const query = '';
+    // TODO Not sure how to use data object here
+    const query = `INSERT INTO genre (genre_id,genre_name) VALUES ('${data}';`;
     return database.query(query);
 }
 
 exports.readAll = async function() {
-    // TODO Avery
-    const query = '';
+    const query = 'SELECT genre_id,genre_name FROM genre;';
     return database.query(query);
 }
 
 exports.readOne = async function(id) {
-    // TODO Avery
-    const query = '';
+    const query = `SELECT genre_id,genre_name FROM genre WHERE genre_id = '${id}';`;
     return database.query(query);
 }
 
 exports.updateOne = async function(id, data) {
-    // TODO Avery
-    const query = '';
+    // TODO Not sure how to use the data and ID objects
+    const query = `UPDATE genre SET genre_id = '${data}', genre_name = '${data}' WHERE genre_id = '${id}';`
     return database.query(query);
 }
 
 exports.destroyOne = async function(uri) {
-    // TODO Avery
-    const query = '';
+    // TODO how to use uri object
+    const query = `DELETE FROM genre WHERE genre_id = '${uri}'`;
     return database.query(query);
 }
 
@@ -39,7 +37,7 @@ exports.destroyOne = async function(uri) {
 ///////////////
 
 exports.getArtists = async function(id) {
-    // TODO Avery
-    const query = '';
+    // TODO use id object correctly
+    const query = `SELECT * FROM genre_artist AS ga JOIN artist AS a USING (artist_uri) WHERE ga.genre_id = '${id}';`;
     return database.query(query);
 }
--- src/model/track.js
+++ src/model/track.js
@@ -5,32 +5,30 @@ const database = require ('../database');
 //////////
 
 exports.create = async function(data) {
-    // TODO Avery
-    const query = '';
+    // TODO use data object correctly
+    const query = `INSERT INTO track VALUES ('${data}');`;
     return database.query(query);
 }
 
 exports.readAll = async function() {
-    // TODO Avery
-    const query = '';
+    const query = 'SELECT * FROM track;';
     return database.query(query);
 }
 
 exports.readOne = async function(uri) {
-    // TODO Avery
-    const query = '';
+    // TODO use uri object correctly
+    const query = `SELECT * FROM track WHERE track_uri = '${uri}';`;
     return database.query(query);
 }
 
 exports.updateOne = async function(uri, data) {
-    // TODO Avery
-    const query = '';
+    // TODO use object correctly
+    const query = `UPDATE track SET track_uri = '${data}', duration_ms = '${data}', track_type = '${data}', track_name = = '${data}', preview_url = '${data}', energy = '${data}', tempo = '${data}', popularity = '${data}' WHERE track_uri = '${uri}';`;
     return database.query(query);
 }
 
 exports.destroyOne = async function(uri) {
-    // TODO Avery
-    const query = '';
+    const query = `DELETE FROM track WHERE track_uri = '${uri}';`;
     return database.query(query);
 }
 
@@ -40,12 +38,11 @@ exports.destroyOne = async function(uri) {
 
 exports.getPlaylist = async function(uri) {
     // TODO Avery
-    const query = '';
+    const query = `SELECT * from track_playlist AS tp JOIN playlist USING (playlist_uri) WHERE playlist_uri = '${uri}';`;
     return database.query(query);
 }
 
 exports.getArtists = async function(uri) {
-    // TODO Avery
-    const query = '';
+    const query = `SELECT * from track JOIN artist_track USING (track_uri) JOIN artist USING (artist_uri) WHERE track.track_uri ='${uri}';`;
     return database.query(query);
 }
