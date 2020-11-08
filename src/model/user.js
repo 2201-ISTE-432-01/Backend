bd321cd78b5e7f0a01d991c55c6052efbeafbbb0
Eyob Abegaz <70550487+EyobAbegaz@users.noreply.github.com>
2020-11-08 17:00:57 -0500

    create, update, destroy, getplaylist edited

--- src/model/user.js
+++ src/model/user.js
@@ -6,6 +6,8 @@ const database = require('../database');
 
 exports.create = async function(data) {
 	// TODO Avery
+	const query = `INSERT INTO user VALUES (${data});`;
+        return database.query(query);
 }
 
 exports.readAll = async function () {
@@ -15,15 +17,19 @@ exports.readAll = async function () {
 
 exports.readOne = async function(uri) {
 	const query = `SELECT * FROM user WHERE user_uri='${uri}';`
-	return database.query(query)
+	return database.query(query);
 }
 
 exports.updateOne = async function(uri, data) {
 	// TODO Avery
+	const query = `UPDATE user SET user_uri = '${data}', display_name = '${data}';`;
+	return database.query(query);
 }
 
 exports.destroyOne = async function(uri) {
 	// TODO Avery
+	const query = `DELETE FROM user WHERE user_uri = '${uri}';`
+        return database.query(query);
 }
 
 ///////////////
@@ -32,4 +38,6 @@ exports.destroyOne = async function(uri) {
 
 exports.getPlaylists = async function(uri) {
 	// TODO Avery
+	const query = `SELECT * FROM playlist JOIN user using (user_uri) WHERE user_uri = '${uri}';`;
+        return database.query(query);
 }
